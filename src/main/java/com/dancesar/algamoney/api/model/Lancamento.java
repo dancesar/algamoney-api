554eaee63417eec3d34d0980c4c082743954ca32
Danillo <danillo.yamagush@gmail.com>
2020-05-01 23:32:52 -0300

    Validando lançamento com Bean Validation

--- src/main/java/com/dancesar/algamoney/api/model/Lancamento.java
+++ src/main/java/com/dancesar/algamoney/api/model/Lancamento.java
@@ -3,6 +3,7 @@ package com.dancesar.algamoney.api.model;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -22,24 +23,31 @@ public class Lancamento implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
     private String descricao;
 
+    @NotNull
     @Column(name = "data_vencimento")
     private LocalDate dataVencimento;
 
     @Column(name = "data_pagamento")
     private LocalDate dataPagamento;
 
+    @NotNull
     private BigDecimal valor;
     private String observacao;
 
+    @NotNull
     @Enumerated(EnumType.STRING)
     private TipoLancamento tipo;
 
+    @NotNull
     @ManyToOne
     @JoinColumn(name = "id_categoria")
     private Categoria categoria;
 
+    @NotNull
     @ManyToOne
     @JoinColumn(name = "id_pessoa")
     private Pessoas pessoas;
