c596ef13ad4d489ef4d4d3ebfe302ad724cc98e8
Danillo <danillo.yamagush@gmail.com>
2020-05-16 14:23:03 -0300

    chore: criado metodo delete para remover Lançamentos

--- src/main/java/com/dancesar/algamoney/api/resource/LancamentoResource.java
+++ src/main/java/com/dancesar/algamoney/api/resource/LancamentoResource.java
@@ -1,5 +1,29 @@
 package com.dancesar.algamoney.api.resource;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.dancesar.algamoney.api.event.RecursoCriadoEvent;
 import com.dancesar.algamoney.api.exceptionhandler.AlgamoneyExceptionHandler;
 import com.dancesar.algamoney.api.model.Lancamento;
@@ -7,19 +31,6 @@ import com.dancesar.algamoney.api.repository.LancamentoRepository;
 import com.dancesar.algamoney.api.repository.filter.LancamentoFilter;
 import com.dancesar.algamoney.api.service.LancamentoService;
 import com.dancesar.algamoney.api.service.exception.PessoaInexistenteOuInativoException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletResponse;
-import javax.validation.Valid;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/lancamentos")
@@ -50,13 +61,14 @@ public class LancamentoResource {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public ResponseEntity<Lancamento> criarLancamento(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response){
         Lancamento lancamnentoSalvo = lancamentoService.salvar(lancamento);
         publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamnentoSalvo.getId()));
         return ResponseEntity.status(HttpStatus.CREATED).body(lancamnentoSalvo);
     }
 
-    @DeleteMapping("{/id}")
+    @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void remover(@PathVariable Long id){
         this.lancamentoRepository.deleteById(id);
