910c533ed2626bf8034913661162b7f1b10ca572
Danillo <danillo.yamagush@gmail.com>
2020-05-20 20:04:29 -0300

    chore: renovando token com refresh_token

--- src/main/java/com/dancesar/algamoney/api/config/AuthorizationServerConfig.java
+++ src/main/java/com/dancesar/algamoney/api/config/AuthorizationServerConfig.java
@@ -24,12 +24,20 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory().withClient("angular").secret("@ngul@r0").scopes("read", "write").authorizedGrantTypes("password").accessTokenValiditySeconds(1800);
+        clients.inMemory().withClient("angular")
+                          .secret("@ngul@r0")
+                          .scopes("read", "write")
+                          .authorizedGrantTypes("password", "refresh_token")
+                          .accessTokenValiditySeconds(1800)
+                          .refreshTokenValiditySeconds(3600 * 24);
     }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.tokenStore(tokenStore()).accessTokenConverter(accessTokenConverter()).authenticationManager(authenticationManager);
+        endpoints.tokenStore(tokenStore())
+                 .accessTokenConverter(accessTokenConverter())
+                 .reuseRefreshTokens(false)
+                 .authenticationManager(authenticationManager);
     }
 
     @Bean
